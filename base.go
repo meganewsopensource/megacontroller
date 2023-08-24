ea7b3c7a8bb133899a2cfed68bbc03bccbd587b3
Guilherme Oliveira <guilherme.oliveira@meganews.com.br>
2023-08-24 15:17:04 -0300

    Um pequeno passo para a humanidade, um grande passo para o Bendego

---
+++ base.go
@@ -0,0 +1,46 @@
+package controller
+
+import (
+	"github.com/invopop/jsonschema"
+	"net/http"
+)
+
+type ResponseJsonParser interface {
+	JSON(code int, obj any)
+}
+
+type H map[string]any
+
+type Base struct{}
+
+func (u Base) IncorrectSchema(c ResponseJsonParser, entity interface{}) {
+	c.JSON(http.StatusUnprocessableEntity, H{
+		"status":  "error",
+		"message": "incorrect schema",
+		"schema":  jsonschema.Reflect(entity),
+	})
+}
+
+func (u Base) EmptyQueryParameter(c ResponseJsonParser, queryParameter string) {
+	c.JSON(http.StatusBadRequest, H{
+		"status":    "error",
+		"message":   "empty query parameter",
+		"parameter": queryParameter,
+	})
+}
+
+func (u Base) NotIntegerQueryParameter(c ResponseJsonParser, queryParameter string) {
+	c.JSON(http.StatusBadRequest, H{
+		"status":    "error",
+		"message":   "not integer query parameter",
+		"parameter": queryParameter,
+	})
+}
+
+func (u Base) EmptyHeader(c ResponseJsonParser, header string) {
+	c.JSON(http.StatusBadRequest, H{
+		"status":  "error",
+		"message": "empty header",
+		"header":  header,
+	})
+}
